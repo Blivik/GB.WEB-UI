bbd0d5c3e550251beb22f6548007f5c6b9392c54
Victoria <victoria.blivernitz@gmail.com>
2021-12-04 17:32:21 +0100

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    { assertTrue (true); }
+
+    private void assertTrue(boolean condition) {
+    }
+}
