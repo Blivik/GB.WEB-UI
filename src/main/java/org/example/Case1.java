f7a79e38858763aa56abcc8d7eac7b5a95cac61a
Victoria <victoria.blivernitz@gmail.com>
2021-12-08 00:28:53 +0100

    Добавила второй тестовый сценарий

---
+++ src/main/java/org/example/Case1.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Case1 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+        driver.get("https://livejournal.com/");
+
+        WebElement Enter = driver.findElement(By.xpath("//*[@id=\"js\"]/body/div[2]/header/div[1]/nav[2]/ul/li[2]/a"));
+        Enter.click();
+        WebElement Login = driver.findElement(By.id("user"));
+        Login.click();
+        Login.sendKeys("VBGB");
+        WebElement Pass = driver.findElement(By.id("lj_loginwidget_password"));
+        Pass.click();
+        Pass.sendKeys("kA7Z2FACxr");
+        driver.quit();
+    }
+}
--- src/main/java/org/example/Case2.java
+++ src/main/java/org/example/Case2.java
@@ -5,13 +5,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class App {
+import javax.swing.*;
+import java.time.Duration;
+
+public class Case2 {
     public static void main(String[] args)
     {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().fullscreen();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
         driver.get("https://livejournal.com/");
+
         WebElement webElement = driver.findElement(By.xpath("//*[@id=\"js\"]/body/div[2]/header/div[1]/nav[2]/ul/li[2]/a"));
         webElement.click();
         WebElement webElement1 = driver.findElement(By.id("user"));
@@ -24,7 +36,11 @@ public class App {
         webElement3.click();
         WebElement webElement4 = driver.findElement(By.xpath("//div[2]/form/button"));
         webElement4.click();
-        WebElement webElement5 = driver.findElement(By.xpath("//*[@class='s-header-item-post--long']"));
+
+       WebElement searchBtn = driver.findElement(By.xpath("//span[@class='s-header-item-post--short']"));
+       WebElement webElement5 = driver.findElement(By.xpath("//span[@class='s-header-item-post--short']"));
+       new WebDriverWait(driver, Duration.ofSeconds(10))
+               .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='s-header-item-post--short']")));
         webElement5.click();
         WebElement webElement6 = driver.findElement(By.xpath("//*[@class='_xt']"));
         webElement6.click();
